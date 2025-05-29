a1d9d2d35e18039190e741768d92aca68380906c
Khassan <tkhassanr@gmail.com>
2025-05-29 12:28:05 +0500

    uncommented

--- task1.go
+++ task1.go
@@ -2,27 +2,27 @@ package main
 
 import "fmt"
 
-// func myAdd(start int, num int) int {
-// 	answer := start
-// 	for i := 1; i <= num; i++ {
-// 		temp := answer
-// 		for temp != 0 {
-// 			answer += temp % 10
-// 			temp /= 10
-// 		}
-// 	}
-// 	return answer
-// }
+func myAdd(start int, num int) int {
+	answer := start
+	for i := 1; i <= num; i++ {
+		temp := answer
+		for temp != 0 {
+			answer += temp % 10
+			temp /= 10
+		}
+	}
+	return answer
+}
 
-// func Fibonnaci(n int) int {
-// 	if n <= 0 {
-// 		return 0
-// 	} else if n == 1 {
-// 		return 1
-// 	} else {
-// 		return Fibonnaci(n-1) + Fibonnaci(n-2)
-// 	}
-// }
+func Fibonnaci(n int) int {
+	if n <= 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	} else {
+		return Fibonnaci(n-1) + Fibonnaci(n-2)
+	}
+}
 
 func pyramid(rows int) {
 	for i := 1; i <= rows; i++ {
@@ -46,13 +46,13 @@ func pyramid(rows int) {
 
 func main() {
 
-	// var number int
-	// var iter int
-	// fmt.Scan(&number, &iter)
+	var number int
+	var iter int
+	fmt.Scan(&number, &iter)
 
-	// fmt.Println(myAdd(number, iter))
+	fmt.Println(myAdd(number, iter))
 
-	// fmt.Println(Fibonnaci(12))
+	fmt.Println(Fibonnaci(12))
 	var rows int
 	fmt.Scan(&rows)
 	pyramid(rows)
